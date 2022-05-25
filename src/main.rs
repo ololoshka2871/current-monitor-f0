5160b80cf3b6d2592e2897dd7d8c2b20d1ec2ee1
ololoshka2871 <SweetTreasure@2ch.hk>
2022-05-25 15:22:45 +0300

    hid report

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,23 @@
 
 extern crate alloc;
 
+mod report;
 mod support;
 
 use cortex_m_rt::entry;
 
+use hal::time::Hertz;
 // без этого будет ошибка отсутвия векторов прерываний
 use stm32f0xx_hal as hal;
 
-use hal::stm32f0::stm32f0x2::Interrupt;
-use hal::{i2c::I2c, pac, pac::interrupt, prelude::*};
+use hal::timers::Timer;
+use hal::{i2c::I2c, pac, pac::interrupt, prelude::*, usb::Peripheral};
 
 use ina219::{INA219, INA219_ADDR};
+
+use stm32_usbd::UsbBus;
+use usb_device::{class_prelude::UsbBusAllocator, device::UsbDevice};
+use usbd_hid::{descriptor::SerializedDescriptor, hid_class::HIDClass};
 
 //-------------------------------------------------------
 
@@ -24,6 +30,10 @@ static ALLOCATOR: alloc_cortex_m::CortexMHeap = alloc_cortex_m::CortexMHeap::emp
 
 //-------------------------------------------------------
 
+static mut USB_BUS: Option<UsbBusAllocator<UsbBus<Peripheral>>> = None;
+static mut USB_DEV: Option<UsbDevice<UsbBus<Peripheral>>> = None;
+static mut HID: Option<HIDClass<UsbBus<Peripheral>>> = None;
+
 #[entry]
 fn main() -> ! {
     defmt::info!("++ Startup ++");
@@ -31,8 +41,8 @@ fn main() -> ! {
     // Initialize the allocator BEFORE you use it
     init_heap();
 
-    let (i2c, usb_bus, nvic) =
-        if let (Some(p), Some(dp)) = (cortex_m::Peripherals::take(), pac::Peripherals::take()) {
+    let (i2c, nvic, mut timer) =
+        if let (Some(cp), Some(dp)) = (cortex_m::Peripherals::take(), pac::Peripherals::take()) {
             cortex_m::interrupt::free(move |cs| {
                 let mut flash = dp.FLASH;
 
@@ -62,9 +72,13 @@ fn main() -> ! {
                     pin_dp: gpioa.pa12,
                 };
 
-                let usb_bus = hal::usb::UsbBus::new(usb);
+                unsafe {
+                    USB_BUS = Some(hal::usb::UsbBus::new(usb));
+                }
 
-                (i2c, usb_bus, p.NVIC)
+                let timer = Timer::tim14(dp.TIM14, Hertz(10), &mut rcc);
+
+                (i2c, cp.NVIC, timer)
             })
         } else {
             defmt::error!("Failed to take peripherials!");
@@ -76,95 +90,92 @@ fn main() -> ! {
         INA219_ADDR - 1, // A0, A1 == 0
     );
 
+    defmt::info!("Calibration...");
     if let Err(e) = ina.calibrate(0x0000) {
         defmt::error!("Calibration failed: {}", defmt::Debug2Format(&e));
         panic!();
     }
 
-    let mut serial = usbd_serial::SerialPort::new(&usb_bus);
+    defmt::info!("Register hid interface");
+    unsafe {
+        HID = Some(HIDClass::new(
+            USB_BUS.as_ref().unwrap_unchecked(),
+            report::Ina219Report::desc(),
+            60,
+        ));
+    }
 
-    let mut usb_dev = usb_device::device::UsbDeviceBuilder::new(
-        &usb_bus,
-        usb_device::device::UsbVidPid(0x0483, 0x5789),
-    )
-    .manufacturer("Shilo_XyZ_")
-    .product("Fast Ampermeter")
-    .serial_number("0000001")
-    .device_class(usbd_serial::USB_CLASS_CDC)
-    .build();
+    defmt::info!("Register USB device");
+    unsafe {
+        USB_DEV = Some(
+            usb_device::device::UsbDeviceBuilder::new(
+                USB_BUS.as_ref().unwrap_unchecked(),
+                usb_device::device::UsbVidPid(0x0483, 0x5789),
+            )
+            .manufacturer("Shilo_XyZ_")
+            .product("Fast Ampermeter")
+            .serial_number("0000001")
+            .build(),
+        );
+    }
 
     unsafe {
+        const SCB_SCR_SEVONPEND: u32 = 1 << 4;
+
         let c: *mut cortex_m::peripheral::NVIC = &nvic as *const _ as *mut _;
-        (*c).set_priority(Interrupt::USB, 1)
+        (*c).set_priority(interrupt::USB, 1);
+
+        // exit wfe() on eny HW event
+        (*cortex_m::peripheral::SCB::ptr())
+            .scr
+            .modify(|scr| scr | SCB_SCR_SEVONPEND);
+        //(*c).set_priority(interrupt::TIM14, 2);
     }
 
     loop {
-        /*
-        let voltage = ina.voltage().unwrap();
-        defmt::info!("bus voltage: {}", voltage);
+        // block until usb interrupt
+        // enable usb interrupt
+        unsafe { cortex_m::peripheral::NVIC::unmask(interrupt::USB) };
 
-        let shunt_voltage = ina.shunt_voltage().unwrap();
-        defmt::info!("shunt voltage: {}", shunt_voltage);
+        // sleep
+        #[cfg(not(debug_assertions))]
+        cortex_m::asm::wfe();
 
-        let current = ina.current().unwrap();
-        defmt::info!("current: {}", current);
+        // disable usb interrupt
+        cortex_m::peripheral::NVIC::mask(interrupt::USB);
 
-        let power = ina.power().unwrap();
-        defmt::info!("power: {}", power);
-        */
-
-        // Важно! Список передаваемый сюда в том же порядке,
-        // что были инициализированы интерфейсы
-        let res = usb_dev.poll(&mut [&mut serial]);
-
-        if res {
-            serial_echo(&mut serial);
-        } else {
-            // block until usb interrupt
-            // enable usb interrupt
-            unsafe { cortex_m::peripheral::NVIC::unmask(Interrupt::USB) };
-
-            // sleep (wfi() почему-то не работает.)
-            cortex_m::asm::wfe();
-
-            // disable usb interrupt
-            cortex_m::peripheral::NVIC::mask(Interrupt::USB);
-        }
-    }
-}
-
-fn serial_echo<B: usb_device::bus::UsbBus>(serial: &mut usbd_serial::SerialPort<B>) {
-    let mut buf = [0u8; 64];
-
-    match serial.read(&mut buf) {
-        Ok(count) if count > 0 => {
-            // Echo back in upper case
-            for c in buf[0..count].iter_mut() {
-                if 0x61 <= *c && *c <= 0x7a {
-                    *c &= !0x20;
-                }
-            }
-
-            let mut write_offset = 0;
-            while write_offset < count {
-                match serial.write(&buf[write_offset..count]) {
-                    Ok(len) if len > 0 => {
-                        write_offset += len;
+        if timer.wait().is_ok() {
+            if let Some(_hid) = unsafe { HID.as_mut() } {
+                match report::Ina219Report::new(&mut ina) {
+                    Ok(report) => {
+                        if let Err(e) = report.push(_hid) {
+                            defmt::error!("USB error: {}", defmt::Debug2Format(&e))
+                        }
                     }
-                    _ => {}
+                    Err(e) => defmt::error!("I2C error: {}", defmt::Debug2Format(&e)),
                 }
             }
         }
-        _ => {}
     }
 }
 
 #[interrupt]
 unsafe fn USB() {
-    // путое прерывание, просто чтобы проц проснулся
+    if let (Some(usb_dev), Some(hid)) = (USB_DEV.as_mut(), HID.as_mut()) {
+        // Важно! Список передаваемый сюда в том же порядке,
+        // что были инициализированы интерфейсы
+        usb_dev.poll(&mut [hid]);
+    }
 
-    cortex_m::peripheral::NVIC::mask(Interrupt::USB);
-    cortex_m::peripheral::NVIC::unpend(Interrupt::USB);
+    cortex_m::peripheral::NVIC::mask(interrupt::USB);
+    cortex_m::peripheral::NVIC::unpend(interrupt::USB);
+}
+
+#[interrupt]
+unsafe fn TIM14() {
+    // just disable interrupt, dont clear flag in timer
+    cortex_m::peripheral::NVIC::mask(interrupt::TIM14);
+    cortex_m::peripheral::NVIC::unpend(interrupt::TIM14);
 }
 
 fn init_heap() {
---
+++ src/report.rs
@@ -0,0 +1,51 @@
+use embedded_hal::blocking::i2c;
+use ina219::INA219;
+use serde::ser::{Serialize, SerializeTuple, Serializer};
+use usb_device::UsbError;
+use usbd_hid::{
+    descriptor::{AsInputReport, SerializedDescriptor},
+    hid_class::HIDClass,
+};
+use usbd_hid_macros::gen_hid_descriptor;
+
+/// Ina219 input report (RAW values)
+#[gen_hid_descriptor(
+    (collection = APPLICATION, usage_page = VENDOR_DEFINED_START, usage = 0x01) = {
+        voltage=input;
+        shunt_voltage=input;
+        current=input;
+        power=input;
+     }
+)]
+#[allow(dead_code)]
+pub struct Ina219Report {
+    pub voltage: u16,
+    pub shunt_voltage: i16,
+    pub current: i16,
+    pub power: i16,
+}
+
+impl Ina219Report {
+    pub fn new<I2C, E>(ina: &mut INA219<I2C>) -> Result<Self, E>
+    where
+        I2C: i2c::Write<Error = E> + i2c::Read<Error = E>,
+    {
+        Ok(Self {
+            voltage: ina.voltage()?,
+            shunt_voltage: ina.shunt_voltage()?,
+            current: ina.current()?,
+            power: ina.power()?,
+        })
+    }
+
+    pub fn push<B>(&self, hid: &mut HIDClass<B>) -> usb_device::Result<usize>
+    where
+        B: usb_device::bus::UsbBus,
+    {
+        match cortex_m::interrupt::free(move |_| hid.push_input(self)) {
+            Ok(r) => Ok(r),
+            Err(UsbError::WouldBlock) => Ok(0),
+            Err(e) => Err(e),
+        }
+    }
+}
